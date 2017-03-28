7943e3c623b8201d823292cbcd57b1afd487dfa8
LucasBorboleta <lucas.borboleta@free.fr>
2017-03-28 23:50:34 +0200

    Still designing

--- doc/Design.js
+++ doc/Design.js
@@ -4,28 +4,136 @@
 // This files just helps thinking about the design.
 
 ///////////////////////////////////////////////////////////////////////////////
-// Study of abstract interaction between the RulesEngine and the player.
+// Study of abstract interaction between the RulesEngine and the two Players.
 // The GUI aspect is not considered here.
 
-// Initialization sequence
+// Create the two Players
+// All kinds of concrete class inherits the class Baltek.Player
+var bluePlayer = new Baltek.HumanPlayer();
+var redPlayer = new Baltek.AiPlayer();
+var redPlayer = new Baltek.RemotePlayer(); // Also possible
+// etc
+
+// Create a new RulesEngine for each new match
 var rulesEngine = new Baltek.RulesEngine();
-var bluePlayer = new Baltek.Player();
-var redPlayer = new Baltek.Player();
 
-rulesEngine.setBluePlayer(bluePlayer);
-rulesEngine.setBluePlayer(redPlayer);
+// Connect the two Players on the RulesEngine
+rulesEngine.connectBluePlayer(bluePlayer);
+rulesEngine.connectBluePlayer(redPlayer);
 
-// Other settings ...
+// Init the match
+rulesEngine.matchInit();
 
 // Start the interactions
-rulesEngine.run();
+rulesEngine.update();
 
-// Let us concentrate on the blue player
+///////////////////////////////////////////////////////////////////////////////
+
+Baltek.RulesEngine.prototype.matchInit = function(){
+    // ...
+
+    // The blue player always starts the first trun
+    this.activePlayer = this.bluePlayer;
+    this.passivePlayer = this.redPlayer;
+    this.blueScore = 0;
+    this.redScore = 0;
+    this.match.isActive = true;
+    this.roundInit();
+}
+
+Baltek.RulesEngine.prototype.roundInit = function(){
+    // Prepare the footballers and the ball for the kickoff
+
+    this.goal = false;
+    this.round.isActive = true;
+    this.turnInit();
+}
+
+Baltek.RulesEngine.prototype.turnInit = function(){
+    this.turn.credit = CREDIT_MAX;
+    this.turn.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.moveInit = function(){
+    this.move.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.update = function(){
+    var SCORE_MAX = 2;
+    var CREDIT_MAX = 3;
+
+    if ( this.blueScore >= SCORE_MAX or this.redScore >= SCORE_MAX ) {
+        this.match.isActive = false;
+    }
+
+    if ( this.goal !== null ) {
+        this.round.isActive = false;
+    }
+
+    if ( this.turn.credit <= 0 ) {
+        this.turn.isActive = false;
+    }
+
+    if ( this.match.isActive ) {
+
+        if ( this.round.isActive ) {
+
+            if ( this.turn.isActive ) {
+
+                if ( this.move.isActive ) {
+                    // Update the possible moves, broken in three steps
+                    // Notify the activePlayer
+
+                } else {
+                    // this.move.isActive === false
+                    this.moveInit();
+                }
+
+            } else {
+                // this.turn.isActive === false
+                this.switchActiveAndPassivePlayers();
+                this.turnInit();
+            }
+
+        } else {
+            // this.round.isActive === false
+            this.switchActiveAndPassivePlayers();
+            this.roundInit();
+        }
+
+    } else {
+        // this.match.isActive === false
+    }
+}
+
+Baltek.RulesEngine.prototype.switchActiveAndPassivePlayers = function(){
+    var oldActivePlayer = this.activePlayer;
+    var oldPassivePlayer = this.passivePlayer;
+    this.activePlayer = this.oldPassivePlayer;
+    this.passivePlayer = this.oldActivePlayer;
+}
+
+Baltek.RulesEngine.prototype.setActivePlayer = function(activePlayer){
+    if ( activePlayer !== this.activePlayer ) {
+        this.switchActiveAndPassivePlayers();
+    }
+}
+
+Baltek.RulesEngine.prototype.setPassivePlayer = function(passivePlayer){
+    if ( passivePlayer !== this.passivePlayer ) {
+        this.switchActiveAndPassivePlayers();
+    }
+}
 
 Baltek.RulesEngine.prototype.requestMove = function(player){
     var move = new Baltek.RulesEngine.Move();
     // etc
     player.buildMove(move);
+}
+///////////////////////////////////////////////////////////////////////////////
+Baltek.Player.prototype.playTurn = function(){
+
+
 }
 
 Baltek.Player.prototype.buildMove = function(move){
@@ -33,7 +141,7 @@ Baltek.Player.prototype.buildMove = function(move){
     var sourceBox = null;
     // TODO: decision of human or AI.
     move.setSourceBox(sourceBox);
-    
+
     var selectableMoveKinds = move.getSelectableMoveKinds();
     var moveKind = null;
     // TODO: decision of human or AI.
