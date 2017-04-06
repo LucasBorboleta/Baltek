a5e04bec7d735afb94ad87dc3bc1e136affec685
LucasBorboleta <lucas.borboleta@free.fr>
2017-04-06 18:26:18 +0200

    Team classes better used

--- doc/Design.js
+++ doc/Design.js
@@ -25,196 +25,23 @@ rulesEngine.connectBluePlayer(redPlayer);
 rulesEngine.matchInit();
 
 // Start the interactions
-rulesEngine.update();
+rulesEngine.matchUpdate();
 
 ///////////////////////////////////////////////////////////////////////////////
 
 Baltek.RulesEngine.Team.prototype.$init = function(){
-    this.player = null;
-    this.score = 0;
+    this.color = ... ; // Blue or Red
     this.footballers = null;
     this.goalDestination = null;
+    this.player = null;
+    this.score = 0;
+    this.canSprint = false;
+    this.haveGoaled = false;
+    this.credit = 0;
 }
 
-///////////////////////////////////////////////////////////////////////////////
+//TODO: Team.prepareFootballers(kickoff);
 
-Baltek.RulesEngine.prototype.connectBluePlayer = function(bluePlayer){
-    this.blueTeam.player = bluePlayer;
-}
-
-Baltek.RulesEngine.prototype.connectRedPlayer = function(redPlayer){
-    this.redTeam.player = redPlayer;
-}
-
-Baltek.RulesEngine.prototype.matchInit = function(){
-    // ...
-
-    // The blue player always starts the first trun
-    this.activeTeam = this.blueTeam;
-    this.passiveTeam = this.redTeam;
-    this.blueTeam.score = 0;
-    this.redTeam.score = 0;
-    this.match.isActive = true;
-    this.roundInit();
-}
-
-Baltek.RulesEngine.prototype.roundInit = function(){
-    // Prepare the footballers and the ball for the kickoff
-
-    this.round.goal = false;
-    this.round.isActive = true;
-    this.turnInit();
-}
-
-Baltek.RulesEngine.prototype.turnInit = function(){
-    var CREDIT_MAX = 3;
-    this.turn.credit = CREDIT_MAX;
-    this.turn.isActive = true;
-    this.moveInit();
-}
-
-Baltek.RulesEngine.prototype.moveInit = function(){
-    this.move.isActive = true;
-    
-    this.move.cost = 0;
-
-    this.move.sourceIsSelected = false;
-    this.move.sources = null;
-    this.move.selectedSource = null;
-
-    this.move.kindIsSelected = false;
-    this.move.kinds = null;
-    this.move.selectedKind = null;
-
-    this.move.destinationIsSelected = false;
-    this.move.destinations = null;
-    this.move.selectedDestination = null;
-}
-
-Baltek.RulesEngine.prototype.moveFindSources = function(){
-    // ... and update this.move.sources
-    this.move.sources = ... ;
-}
-
-Baltek.RulesEngine.prototype.moveSelecSource = function(source){
-    if ( this.move.isActive ) {
-        if ( ! this.move.sourceIsSelected) {
-            if ( Baltek.Utils.hasValue(this.move.sources, source) ) {
-                this.move.selectedSource = source;
-                this.move.sourceIsSelected = true;
-                this.update();
-            }
-        }
-    }
-}
-
-Baltek.RulesEngine.prototype.moveFindKinds = function(){
-    // ... and update this.move.kinds
-    this.move.kinds = ... ;
-}
-
-Baltek.RulesEngine.prototype.moveSelectKind = function(kind){
-    if ( this.move.isActive && this.move.sourceIsSelected ) {
-        if ( ! this.move.kindIsSelected ) {
-            if ( Baltek.Utils.hasValue(this.move.kinds, kind) ) {
-                this.move.selectedKind = kind;
-                this.move.kindIsSelected = true;
-                this.update();
-            }
-        }
-    }
-}
-
-Baltek.RulesEngine.prototype.moveFindDestinations = function(){
-    // ... and update this.move.destinations
-    this.move.destinations = ... ;
-}
-
-Baltek.RulesEngine.prototype.moveSelectDestination = function(destination){
-    if ( this.move.isActive && this.move.sourceIsSelected && this.move.kindIsSelected ) {
-        if ( ! this.move.destinationIsSelected ) {
-            if ( Baltek.Utils.hasValue(this.move.destinations, destination) ) {
-                this.move.selectedDestination = destination;
-                this.move.destinationIsSelected = true;
-                this.update();
-            }
-        }
-    }
-}
-
-Baltek.RulesEngine.prototype.update = function(){
-
-    if ( this.match.isActive ) {
-
-        if ( this.round.isActive ) {
-
-            if ( this.turn.isActive ) {
-
-                if ( this.move.isActive ) {
-
-                    if ( this.move.sourceIsSelected ) {
-
-                        if ( this.move.kindIsSelected ) {
-
-                            if ( this.move.destinationIsSelected ) {
-                                this.move.isActive = false;
-
-                                // Compute the cost of the move
-                                this.turn.credit -= this.move.cost;
-                                this.turn.isActive = ( this.turn.credit <= 0 );
-
-                                // Check goal
-                                this.round.goal = (this.move.selectedDestination == this.activeTeam.goalDestination)
-                                if ( this.round.goal ) {
-                                    this.round.isActive = false;
-
-                                    this.activeTeam.score += 1
-
-                                    var SCORE_MAX = 2;
-
-                                    if ( this.activeTeam.score >= SCORE_MAX or this.passiveTeam.score >= SCORE_MAX ) {
-                                        this.match.isActive = false;
-                                    }
-                                }
-
-                            } else {
-                                // this.move.destinationIsSelected === false
-                                this.moveFindDestinations();
-                                // notify the activeTeam
-                            }
-
-                        } else {
-                            // this.move.kindIsSelected === false
-                            this.moveFindKinds();
-                            // notify the activeTeam
-                        }
-                    } else {
-                        // this.move.sourceIsSelected === false
-                        this.moveFindSources();
-                        // notify the activeTeam
-                    }
-
-                } else {
-                    // this.move.isActive === false
-                    this.moveInit();
-                }
-
-            } else {
-                // this.turn.isActive === false
-                this.switchActiveAndPassiveTeams();
-                this.turnInit();
-            }
-
-        } else {
-            // this.round.isActive === false
-            this.switchActiveAndPassiveTeams();
-            this.roundInit();
-        }
-
-    } else {
-        // this.match.isActive === false
-    }
-}
 
 Baltek.RulesEngine.prototype.switchActiveAndPassiveTeams = function(){
     var oldActiveTeam = this.activeTeam;
@@ -232,6 +59,244 @@ Baltek.RulesEngine.prototype.setActiveTeam = function(activeTeam){
 Baltek.RulesEngine.prototype.setPassiveTeam = function(passiveTeam){
     if ( passiveTeam !== this.passiveTeam ) {
         this.switchActiveAndPassiveTeams();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+Baltek.RulesEngine.prototype.connectBluePlayer = function(bluePlayer){
+    this.blueTeam.player = bluePlayer;
+}
+
+Baltek.RulesEngine.prototype.connectRedPlayer = function(redPlayer){
+    this.redTeam.player = redPlayer;
+}
+
+Baltek.RulesEngine.prototype.matchInit = function(){
+    this.activeTeam = this.blueTeam;
+    this.passiveTeam = this.redTeam;
+
+    this.activeTeam.score = 0;
+    this.passiveTeam.score = 0;
+
+    this.roundInit();
+    this.match.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.roundInit = function(){
+    this.activeTeam.canSprint = true;
+    this.passiveTeam.canSprint = true;
+
+    this.activeTeam.haveGoaled = false;
+    this.passiveTeam.haveGoaled = false;
+
+    var kickoff = true;
+    this.activeTeam.prepareFootballers(kickoff);
+    this.passiveTeam.prepareFootballers(! kickoff);
+
+    this.turnInit();
+    this.round.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.turnInit = function(){
+    var CREDIT_MAX = 3;
+    this.activeTeam.credit = CREDIT_MAX;
+    this.passiveTeam.credit = 0;
+
+    this.moveInit();
+    this.turn.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.moveInit = function(){
+    this.move.cost = 0;
+
+    this.move.sourceIsSelected = false;
+    this.move.sources = null;
+    this.move.selectedSource = null;
+
+    this.move.kindIsSelected = false;
+    this.move.kinds = null;
+    this.move.selectedKind = null;
+
+    this.move.destinationIsSelected = false;
+    this.move.destinations = null;
+    this.move.selectedDestination = null;
+
+    this.move.isActive = true;
+}
+
+Baltek.RulesEngine.prototype.matchUpdate = function(){
+    if ( this.match.isActive ) {
+        this.roundUpdate();
+
+        if ( ! this.round.isActive ) {
+
+            if ( this.activeTeam.haveGoaled ) {
+                this.activeTeam.score += 1
+
+                var SCORE_MAX = 2;
+                if ( this.activeTeam.score >= SCORE_MAX or this.passiveTeam.score >= SCORE_MAX ) {
+                    this.match.isActive = false;
+                }
+            }
+
+            if ( this.match.isActive ) {
+                this.switchActiveAndPassiveTeams();
+                this.roundInit();
+            }
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.roundUpdate = function(){
+    if ( this.round.isActive ) {
+
+        this.turnUpdate();
+
+        if ( ! this.turn.isActive ) {
+
+            if ( this.activeTeam.haveGoaled ) {
+                this.round.isActive = false;
+            }
+
+            if ( this.round.isActive ) {
+                this.switchActiveAndPassiveTeams();
+                this.turnInit();
+            }
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.turnUpdate = function(){
+    if ( this.turn.isActive ) {
+
+        this.moveUpdate();
+
+        if ( ! this.move.isActive ) {
+
+            if ( this.activeTeam.credit <= 0 ) {
+                this.turn.isActive = false;
+            }
+
+            if ( this.activeTeam.haveGoaled ) {
+                this.turn.isActive = false;
+            }
+
+            if ( this.turn.isActive ) {
+                this.moveInit();
+            }
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.turnStop = function(){
+    // Triggered by a Player
+    if ( this.turn.isActive ) {
+        this.turn.isActive = false;
+        this.matchUpdate();
+    }
+}
+
+Baltek.RulesEngine.prototype.moveUpdate = function(){
+    if ( this.move.isActive ) {
+
+        if ( this.move.sourceIsSelected ) {
+
+            if ( this.move.kindIsSelected ) {
+
+                if ( this.move.destinationIsSelected ) {
+                    this.move.isActive = false;
+
+                    this.activeTeam.credit -= this.move.cost;
+
+                    if (this.move.selectedDestination === this.activeTeam.goalDestination ) {
+                        this.activeTeam.haveGoaled = true;
+                    }
+
+                } else {
+                    // this.move.destinationIsSelected === false
+                    this.moveFindDestinations();
+                    // notify the activeTeam
+                }
+
+            } else {
+                // this.move.kindIsSelected === false
+                this.moveFindKinds();
+                // notify the activeTeam
+            }
+        } else {
+            // this.move.sourceIsSelected === false
+            this.moveFindSources();
+            // notify the activeTeam
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.moveSelectSource = function(source){
+    // Triggered by a Player
+    if ( this.move.isActive ) {
+        if ( ! this.move.sourceIsSelected) {
+            if ( Baltek.Utils.hasValue(this.move.sources, source) ) {
+                this.move.selectedSource = source;
+                this.move.sourceIsSelected = true;
+                this.matchUpdate();
+            }
+        }
+    }
+}
+
+//TODO: moveUnselectSource
+//TODO: moveUnselectDestination
+
+Baltek.RulesEngine.prototype.moveSelectKind = function(kind){
+    // Triggered by a Player
+    if ( this.move.isActive && this.move.sourceIsSelected ) {
+        if ( ! this.move.kindIsSelected ) {
+            if ( Baltek.Utils.hasValue(this.move.kinds, kind) ) {
+                this.move.selectedKind = kind;
+                this.move.kindIsSelected = true;
+                this.matchUpdate();
+            }
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.moveSelectDestination = function(destination){
+    // Triggered by a Player
+    if ( this.move.isActive && this.move.sourceIsSelected && this.move.kindIsSelected ) {
+        if ( ! this.move.destinationIsSelected ) {
+            if ( Baltek.Utils.hasValue(this.move.destinations, destination) ) {
+                this.move.selectedDestination = destination;
+                this.move.destinationIsSelected = true;
+                // Compute the cost of the move
+                this.move.cost = ... ;
+                this.matchUpdate();
+            }
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.moveFindSources = function(){
+    if ( this.move.isActive ) {
+        if ( ! this.move.sourceIsSelected) {
+            this.move.sources = ... ;
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.moveFindKinds = function(){
+    if ( this.move.isActive && this.move.sourceIsSelected ) {
+        if ( ! this.move.kindIsSelected ) {
+            this.move.kinds = ... ;
+        }
+    }
+}
+
+Baltek.RulesEngine.prototype.moveFindDestinations = function(){
+    if ( this.move.isActive && this.move.sourceIsSelected && this.move.kindIsSelected ) {
+        if ( ! this.move.destinationIsSelected ) {
+            this.move.destinations = ... ;
+        }
     }
 }
 
